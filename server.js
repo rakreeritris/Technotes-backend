022d7e1ed9acb27ff2d032e922eb645726d78874
Ritik Gupta <rakreeritris@gmail.com>
2023-10-18 09:37:19 +0530

    added mongodb also

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3500;
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { logger } = require("./middleware/logger");
+const { logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+
+// connecting to DB
+connectDB();
+
+//middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(logger);
+
+//router
+const rootRouter = require("./routes/root");
+
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", rootRouter);
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "The requested resources are not found" });
+  } else if (req.accepts("txt")) {
+    res.type("txt").send("404 not found");
+  }
+});
+app.use(errorHandler);
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
